01118cd82ece00e0484802d5649e2386652699ab
John Van Wagenen <jvanwagenen@mastercontrol.com>
2018-12-19 20:03:19 -0700

    The footer is now hiding like it should.

--- src/app.js
+++ src/app.js
@@ -12,6 +12,14 @@ function createDiv(idPortion) {
     return div;
 }
 
+function createButton(text) {
+    let btn = document.createElement('button');
+    btn.type = 'button';
+    btn.innerText = text;
+
+    return btn;
+}
+
 function getTextForNotice(providedText, urlToTerms) {
     let noticeText = document.createElement('p');
     noticeText.id = 'gdpr-lite-text';
@@ -34,20 +42,28 @@ function getTextForNotice(providedText, urlToTerms) {
 
 function includeCss() {
     let style = document.createElement('style');
-    style.innerText = '#gdpr-lite-toast{visibility:hidden;max-width:50px;height:50px;margin:auto;background-color:#333;color:#fff;text-align:center;border-radius:2px;position:fixed;z-index:1;left:0;right:0;bottom:30px;font-size:17px;white-space:nowrap;}#gdpr-lite-toast#gdpr-lite-img{width:50px;height:50px;float:left;padding-top:16px;padding-bottom:16px;box-sizing:border-box;background-color:#111;color:#fff;}#gdpr-lite-toast#gdpr-lite-desc{color:#fff;padding:16px;overflow:hidden;white-space:nowrap;}#gdpr-lite-toast.show{visibility:visible;-webkit-animation:fadein 0.5s,expand 0.5s 0.5s,stay 3s 1s,shrink 0.5s 2s,fadeout 0.5s 2.5s;animation:fadein 0.5s,expand 0.5s 0.5s,stay 3s 1s,shrink 0.5s 4s,fadeout 0.5s 4.5s;}@-webkit-keyframesfadein{from{bottom:0;opacity:0;}to{bottom:30px;opacity:1;}}@keyframesfadein{from{bottom:0;opacity:0;}to{bottom:30px;opacity:1;}}@-webkit-keyframesexpand{from{min-width:50px}to{min-width:350px}}@keyframesexpand{from{min-width:50px}to{min-width:350px}}@-webkit-keyframesstay{from{min-width:350px}to{min-width:350px}}@keyframesstay{from{min-width:350px}to{min-width:350px}}@-webkit-keyframesshrink{from{min-width:350px;}to{min-width:50px;}}@keyframesshrink{from{min-width:350px;}to{min-width:50px;}}@-webkit-keyframesfadeout{from{bottom:30px;opacity:1;}to{bottom:60px;opacity:0;}}@keyframesfadeout{from{bottom:30px;opacity:1;}to{bottom:60px;opacity:0;}}';
+    style.innerText = '#gdpr-lite-toast{position:fixed;left:0;bottom:0;width:100%;background-color:red;color:white;text-align:center;}';
 
     document.head.appendChild(style);
 }
 
+function wireAcceptEvent(btn) {
+    btn.onclick = (e) => {
+        document.getElementById('gdpr-lite-toast').style.display = 'none';
+    };
+
+    return btn;
+}
+
 function createToastHTML(noticeVerbiage, urlToTerms) {
     let containingDiv = createDiv('toast');
-    let imgDiv = createDiv('img');
     let noticeDiv = createDiv('notice');
     let noticeText = getTextForNotice(noticeVerbiage, urlToTerms);
+    let acceptButton = wireAcceptEvent(createButton('Accept and close'));
 
     noticeDiv.appendChild(noticeText);
-    imgDiv.appendChild(noticeDiv);
-    containingDiv.appendChild(imgDiv);
+    containingDiv.appendChild(noticeDiv);
+    containingDiv.appendChild(acceptButton);
     document.body.appendChild(containingDiv);
 
     includeCss();
@@ -56,8 +72,5 @@ function createToastHTML(noticeVerbiage, urlToTerms) {
 export const gdprLite = {
     initialize(noticeText = 'This site uses cookies to offer its online services.', urlToTerms) {
         createToastHTML(noticeText, urlToTerms);
-        document.addEventListener('DOMContentLoaded', () => {
-            document.getElementById('gdpr-lite-toast').className = 'show';
-        }, false);
     }
 };
